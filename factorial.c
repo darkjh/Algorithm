c9f85e6460c700f1bbd4432e80094f409fa0115e
JH <ju.han.felix.@gmail.com>
2011-07-07 05:07:12 -0700

    Edited factorial.c via GitHub

--- factorial.c
+++ factorial.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int max=5000;
 
@@ -7,51 +8,52 @@ void cal(int num, int* pNow, int* c)
   int sum;
   int ten,one;
 
-  sum=*pNow * num;
-  one=sum%10+(*c);
-  ten=(sum/10);
+  sum=*pNow * num + *c;
+  one=sum%10;
+  ten=(int)(sum/10);
 
-  printf("num:%d sum:%d c:%d one:%d ten:%d\n",num,sum,*c, one, ten);
+  //printf("num:%d sum:%d c:%d one:%d ten:%d\n",num,sum,*c, one, ten);
 
-  if(one>=10)
+  *pNow=one;
+  *c=ten;
+}
+
+void show(int* array, int position)
+{
+    int i;
+    for(i=position;i>=0;i--)
     {
-      one=one%10;
-      ten++;
-    }
-  if(sum<10)
-    {
-      *pNow=one;
-      *c=0;
-    }
-  else
-    {
-      *pNow=one;
-      *c=ten;
+        printf("%d",*(array+i));
     }
 }
 
-
 int main(int argc, char *argv[])
 {
-  int a[7]={0,0,0,0,0,0,0};
+  int a[max];
   int position=0;
   int p=0;
   int i;
   int c=0;
   int limit;
 
-  scanf("%d",&limit);
+  do
+  {
+      scanf("%d",&limit);
+  }while(limit<=0);
+
   a[0]=1;
   for(i=1;i<=limit;i++)
     {
       p=0;
       c=0;
-      while(p<=position)
+      while(p<=position)		//not sure
 	{
 	  cal(i,&a[p],&c);
 	  p++;
 	}
+
       //if(a[position+1]!=0) position++;
+      position=p;
       while(c!=0)
 	{
 	  a[position++]=(int)(c%10);
@@ -59,7 +61,8 @@ int main(int argc, char *argv[])
 	}
     }
 
-  printf("result=%d%d%d%d%d%d%d\n",a[6],a[5],a[4],a[3],a[2],a[1],a[0]);
+    show(a,position-3);
+    free(a);
 
   return 0;
 }
