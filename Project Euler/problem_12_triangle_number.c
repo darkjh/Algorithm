c1a3105c484a73beefb251292ad55615b51bf809
Han JU <ju.han.felix@gmail.com>
2011-07-23 18:46:46 +0200

    up tp q15

---
+++ Project Euler/problem_12_triangle_number.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void strait_forward()
+{
+  //nested fucntion to count one number's factor
+  //It counts only from 2 to sqrt(n), which is a half of all factors
+  int count_factor(int base)
+  {
+    int i;
+    int count=0;
+
+    for(i=2;i<=sqrt(base);i++)
+    {
+        if(base%i==0)
+            count++;
+    }
+    //return factor number
+    return (count+1)*2;
+  }
+
+  //start from 3, constructing the triangle numbers
+    int num=3;
+    int sum=3;
+
+    while(1)
+    {
+        sum+=(num++);
+	//when one number's factors exceed 500, break
+        if(count_factor(sum)>=500)
+            break;
+    }
+    printf("Result: %d",sum);
+}
+
+
+
+
+int main()
+{
+  strait_forward();
+  return 0;
+}
---
+++ Project Euler/problem_13.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main (int argc, char *argv[])
+{
+  FILE *fp;
+  int data[100][50];
+  int result[100];
+  int res;
+  int c;
+  int i, j, k;
+  char waste;
+
+  res = 0;
+  c = 0;
+  k = 0;
+  memset (result, -1, sizeof (char) * 100);
+  fp = fopen ("file_problem_13", "r");
+
+  for (i = 0; i < 100; i++)
+    {
+      for (j = 0; j < 50; j++)
+	{
+	  fscanf (fp, "%c", &waste);
+	  data[i][j] = waste - '0';
+	}
+      fscanf (fp, "%c", &waste);
+    }
+  fclose(fp);
+
+  for (j = 49; j >= 0; j--)
+    {
+      for (i = 0; i < 100; i++)
+	{
+	  res += data[i][j];
+	}
+      res += c;
+      result[k++] = (int) (res % 10);
+      c = (int) (res / 10);
+    }
+  while (c != 0)
+    {
+      result[k++] = (int) (c % 10);
+      c /= 10;
+    }
+
+  i = 0;
+  while (result[i] >= 0)
+    printf ("%d", result[i++]);
+  return 0;
+}
